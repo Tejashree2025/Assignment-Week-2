0ab195caed6349a41f722c773e856442a66c6334
Tejashree2025 <tejashreejuly01@gmail.com>
2025-07-25 21:12:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -1,50 +1,25 @@
 // User function Template for Java
 
 class Solution {
-
+    
+  
+ 
     int countPairs(int arr[], int target) {
-        // Complete the function
         
-        int count =0;
-        int left=0;
-        int right=arr.length-1;
-        int sum=0;
-        
-        while(left<right){
-            sum=arr[left]+arr[right];
-            
-            if(sum<target){
-                left++;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int count = 0;
+
+        for (int num : arr) {
+            int complement = target - num;
+
+            if (map.containsKey(complement)) {
+                count += map.get(complement); // Add how many times complement appeared before
             }
-            else if(sum>target){
-                right--;
-            }
-            else{
-                //sum==target
-                
-                int ele1=arr[left];
-                int ele2=arr[right];
-                int countL=0;
-                int countR=0;
-                
-                while(left<=right && arr[left]==ele1){ 
-                    left++;
-                    countL++;//frequency of left appreances
-                }
-                
-                while(left<=right && arr[right]==ele2){
-                    right--;
-                    countR++;//frequency of left appreances
-                }
-                if(ele1==ele2){
-                    count+=countL*(countL-1)/2;
-                }
-                else{
-                    count+=countL*countR;
-                }
-            }
-            
+
+            // Store current number in the map
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
+
         return count;
     }
 }
