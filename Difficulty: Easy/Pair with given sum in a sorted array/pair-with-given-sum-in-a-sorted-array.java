b2f4e11ac8465a99fd30715052d7886d2fb2b863
Tejashree2025 <tejashreejuly01@gmail.com>
2025-07-26 20:22:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -6,20 +6,49 @@ class Solution {
  
     int countPairs(int arr[], int target) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int count = 0;
+         int res = 0;
+        int n = arr.length;
+        int left = 0, right = n - 1;
 
-        for (int num : arr) {
-            int complement = target - num;
+        while (left < right) {
+          
+            // If sum is greater 
+            if (arr[left] + arr[right] < target)
+                left++;
 
-            if (map.containsKey(complement)) {
-                count += map.get(complement); // Add how many times complement appeared before
+            // If sum is lesser
+            else if (arr[left] + arr[right] > target)
+                right--;
+
+            // If sum is equal
+            else {
+                int cnt1 = 0, cnt2 = 0;
+                int ele1 = arr[left], ele2 = arr[right];
+              
+                // Count frequency of first element of the pair
+                while (left <= right && arr[left] == ele1) {
+                    left++;
+                    cnt1++;
+                }
+              
+                // Count frequency of second element of the pair
+                while (left <= right && arr[right] == ele2) {
+                    right--;
+                    cnt2++;
+                }
+               
+                // If both the elements are same, then count of
+                // pairs = the number of ways to choose 2 
+                // elements among cnt1 elements
+                if (ele1 == ele2) 
+                    res += (cnt1 * (cnt1 - 1)) / 2;
+              
+                // If the elements are different, then count of
+                // pairs = product of the count of both elements
+                else 
+                    res += (cnt1 * cnt2);
             }
-
-            // Store current number in the map
-            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-
-        return count;
+        return res;
     }
 }
